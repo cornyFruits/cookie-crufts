868649dc22bb73da05ffc25b960d835f480f72bd
Krittika Sharma <krittika.sharma@student.unsw.edu.au>
2026-04-25 18:29:56 +1000

    Adding vercel view tracking logic

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+import { inject } from '@vercel/analytics';
+
+inject();
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
